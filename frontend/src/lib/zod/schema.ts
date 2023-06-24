da724d58ca2540ea57c244aa63a304a8323e9ea8
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-06-24 15:58:52 +0200

    fix(utils): cleanup zod schemas

--- frontend/src/lib/zod/schema.ts
+++ frontend/src/lib/zod/schema.ts
@@ -4,10 +4,7 @@ export const loginSchema = z.object({
 	email: z.string(),
 	password: z.string()
 });
-export const passwordReset = z.object({
-	password: z.string(),
-	passwordConfirm: z.string()
-});
+
 export const reqPasswordReset = z.object({
 	email: z.string()
 });
