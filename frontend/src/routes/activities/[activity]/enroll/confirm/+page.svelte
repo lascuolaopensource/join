d19103670db16b9d762d267d1442f9498ac5cbaa
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-07-14 18:36:45 +0200

    feat(enroll): added enroll pages

---
+++ frontend/src/routes/activities/[activity]/enroll/confirm/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let data;
+	const { activity } = data;
+</script>
+
+<h4>{activity.name}</h4>
+<h2>Enrollment succesful!</h2>
+<p>You have been enrolled in the activity. You will receive an email with the details.</p>
