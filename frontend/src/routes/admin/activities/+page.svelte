7839bb750d7fb19575498d86df124fddad13b2df
fcastrovilli <francesco.castrovilli95@gmail.com>
2023-11-09 14:05:30 +0100

    feat: show activities

--- frontend/src/routes/admin/activities/+page.svelte
+++ frontend/src/routes/admin/activities/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import { Plus } from 'lucide-svelte';
+	export let data;
+	const { activities } = data;
 </script>
 
 <div class="flex justify-between items-center">
@@ -9,3 +11,18 @@
 		<span> Create a new activity </span>
 	</a>
 </div>
+{#if activities && activities.length}
+	<div class="grid grid-cols-4 gap-4">
+		{#each activities as activity}
+			<div class="card">
+				<header class="card-header text-xl capitalize">{activity.name}</header>
+				<section class="p-4">
+					{@html activity.description}
+				</section>
+				<footer class="card-footer">
+					<a href="/admin/activities/{activity.id}" class="btn variant-filled">Show</a>
+				</footer>
+			</div>
+		{/each}
+	</div>
+{/if}
